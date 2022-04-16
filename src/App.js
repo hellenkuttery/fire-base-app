cfdc5fafa28ddf4a3037234385ef2e01790c46da
hellenkuttery <helen@clarusway.com>
2022-04-16 13:34:30 +0300

    add main

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import Contacts from './components/contacts/Contacts';
 function App() {
   return (
     <div className="App">
-  <FormComponent/>
-  <Contacts/>
+    <FormComponent/>
+    <Contacts/>
     </div>
   );
 }
