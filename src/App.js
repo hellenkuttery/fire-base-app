30d9687e6a9abc2da1fcdf0f99103ea43ff019c8
hellenkuttery <helen@clarusway.com>
2022-04-16 13:27:26 +0300

    add  files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import FormComponent from './components/form/FormComponent';
+import Contacts from './components/contacts/Contacts';
+
+function App() {
+  return (
+    <div className="App">
+  <FormComponent/>
+  <Contacts/>
+    </div>
+  );
+}
+
+export default App;
